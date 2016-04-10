9fffee4043874393db242652bf046d39ab0f674e
Aaron Thomas <aathomas@redhat.com>
2016-04-10 06:29:22 -0400

    inital check when a player leaves the server was missing

--- savethehoard.sh
+++ savethehoard.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect --
 
-# 24 hour timeout; more than likely will change asap.
+# 24 hour timeout;
 set timeout 86400
 
 # variable for hour / minute for the 'settime' command for the 7 days to die console.
@@ -9,6 +9,10 @@ set minfix 59
 
 # Spawn password-less telnet session to the 7days to die console only allowing localhost connections.
 spawn telnet localhost 8081
+sleep 1
+
+# Expect statement looking for a player leaving.
+expect "left the game"
 sleep 1
 
 # List current players active on the server; 7day's console command 'lp = listplayers'.
@@ -38,7 +42,7 @@ if {$found == 1} {
 } else {put "GOD AARON!\n"}
 sleep 1
 
-# The following checks to see if the day is divisble by seven and if the subsequent feral night hoard is supposed to spawn during the hours  22:00 pm to Midnight. 
+# The following checks to see if the day is divisible by seven and if the subsequent feral night hoard is supposed to spawn during the hours  22:00 pm to Midnight. 
 if {$day % 7 == 0 && $hour >=22} {
     send_user "The world was abondon on a horde night, time will now be reset to Day $day, $hourfix:$minfix"
     send "st $day $hourfix $minfix\n"
@@ -46,7 +50,7 @@ if {$day % 7 == 0 && $hour >=22} {
     send_user {[Day $day % 7 != 0] Not a horde night!\n}
 }
 
-# The following checks to see if the day is divisble by seven minus one and if the subsequent feral night hoard is supposed to spawn during the hours Midnight to 06:00 am.
+# The following checks to see if the day is divisible by seven minus one and if the subsequent feral night hoard is supposed to spawn during the hours Midnight to 06:00 am.
 if {($day - 1) % 7 == 0 && $hour <= 05} {
     send_user "The world was abondon on a horde night, time will now be reset to Day $day, $hourfix:$minfix"
     set day [expr {$day - 1}] 
